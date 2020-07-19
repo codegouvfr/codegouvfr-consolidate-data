4965a3e1cb1fc9e9be2b7c1ae431ba74ce5663f2
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-07-19 22:03:43 +0200

    src/repos.clj: Fix code typo

--- src/repos.clj
+++ src/repos.clj
@@ -91,7 +91,7 @@
              %))
      ;; Add number of reuse
      (map #(if-let [r (not-empty (get reuse (:r %)))]
-             (assoc % :ru (:reuse d))
+             (assoc % :ru (:reuse r))
              %))
      ;; Remap licenses
      (map #(assoc % :li (get licenses-mapping (:li %))))
