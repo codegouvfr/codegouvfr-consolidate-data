a39f283beec387b8d3ebee0a34cfcd28a30043f5
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-07-20 14:19:45 +0200

    src/repos.clj: Fix get reuse (as string, not keyword)

--- src/repos.clj
+++ src/repos.clj
@@ -91,7 +91,7 @@
              %))
      ;; Add number of reuse
      (map #(if-let [r (not-empty (get reuse (:r %)))]
-             (assoc % :g (:reuse r))
+             (assoc % :g (get r "reuse"))
              %))
      ;; Remap licenses
      (map #(assoc % :li (get licenses-mapping (:li %))))
